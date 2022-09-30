7c8f7a69138222e91da1b7e368d065e06745de71
SoccerMomOS <114758718+SoccerMomOS@users.noreply.github.com>
2022-09-30 10:23:47 -0600

    created pre and post boot configuration
    added type values for config
    livecd and regular boot

--- soccermomos/Kernel.cs
+++ soccermomos/Kernel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
 using System.Threading;
+using System.IO;
+using Cosmos.System.FileSystem.Listing;
+using System.Linq;
 
 namespace soccermomos
 {
@@ -11,24 +14,53 @@ namespace soccermomos
 
         protected override void BeforeRun()
         {
+            Console.Clear(); // Clears BSD boot output
+            Console.Write("SMOS!!!!");
+
+            Cosmos.System.FileSystem.FileSystem fileSystem; // Initiates a filesystem type
+
+            Cosmos.System.Network.Config.DNSConfig DNSconfig;
+            Cosmos.System.Network.Config.DNSConfig IPconfig;            // Network Config
+            Cosmos.System.Network.Config.NetworkConfig networkConfig;
+
+            Cosmos.System.KeyEvent keyEvent; // keyboard config event stuff
+        }
+
+
+        protected override void Run()
+        {
             static void beep()
             {
                 Cosmos.System.PCSpeaker.Beep();
                 Thread.Sleep(1);
             }
 
-            Console.Write("SMOS: Makes Life Easy, so you can run a redlight while taking timmy to practice. If you hear 5 beeps, your a cool soccer mom");
-            beep();
-            beep();
-            beep();
-            beep();
-            beep();
-        }
+            var directory_list = Directory.GetFiles(@"0:\"); // Directory list | Code stolen from cosmos
 
-        protected override void Run()
-        {
-            Console.Write("SMOS>>>");
-            var input = Console.ReadLine();
+            List<string> files = new List<string>();
+            foreach (var file in directory_list)    // detects all files and adds them to a list | since the os is light this shouldnt be an issue ruby
+            {
+                files.Add(file);
+            }
+
+            bool listEmpty = !files.Any(); // Checks if list is empty
+            if (listEmpty)
+            {
+                Console.Write("SMOS: Makes Life Easy, so you can run a redlight while taking timmy to practice. If you hear 5 beeps, your a cool soccer mom | Running in liveCD");
+                beep();
+                beep();
+                beep();
+                beep();
+                beep();
+            } else
+            {
+                Console.Write("SMOS: Makes Life Easy, so you can run a redlight while taking timmy to practice. If you hear 5 beeps, your a cool soccer mom");
+                beep();
+                beep();
+                beep();
+                beep();
+                beep();
+            }
         }
     }
 }
